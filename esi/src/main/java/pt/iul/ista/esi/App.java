ed1c44d37c02a17110291b6c6e0de185694ec39e
gongas <gddal@iscte-iul.pt>
2019-12-05 19:01:16 +0000

    My first commit

--- esi/src/main/java/pt/iul/ista/esi/App.java
+++ esi/src/main/java/pt/iul/ista/esi/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
 
-        System.out.println( "Software para analise de erros de software!!!" );
+        System.out.println( "Software para analise de erros de software!" );
 
     }
 }
