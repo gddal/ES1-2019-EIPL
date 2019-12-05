4e9ea3d51b0e8605e72959ac379424e45679d745
Miguel Diaz Gonçalves <mcdgs@iscte-iul.pt>
2019-12-05 22:18:55 +0000

    Alteração do cabeçalho da classe de entrada da aplicação. Configuração do projecto para utilizar o Java 1.8 por defeito.

--- esi/src/main/java/pt/iul/ista/esi/App.java
+++ esi/src/main/java/pt/iul/ista/esi/App.java
@@ -1,9 +1,23 @@
 package pt.iul.ista.esi;
 
 /**
- * Software para analise de erros de software!
- *
- */
+*
+* Trabalho final de Engenharia de Software I 2019/20
+* 
+* Grupo 85
+* 
+* Projecto Analise de erros de software.
+* 
+* Classe de entrada da Aplicação do cliente.
+*
+* @author Miguel Diaz Gonçalves 82493
+* @author Gonçalo Dias do Amaral 83380
+* @author Miguel Carriço 73745
+* @author André Freitas 82361
+* @author Pedro Jones 82946
+* @version 0.01
+*
+*/
 public class App 
 {
     public static void main( String[] args )
