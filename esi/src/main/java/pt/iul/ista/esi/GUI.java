4fa7425d47460fee7ed2d8ebd515dd20dfbab2a7
Utilizador <Utilizador@Desktop>
2019-12-05 21:51:03 +0000

    Feita a estrutura do GUI

---
+++ esi/src/main/java/pt/iul/ista/esi/GUI.java
@@ -0,0 +1,135 @@
+package pt.iul.ista.esi;
+
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.WindowConstants;
+
+
+
+public class GUI extends JPanel{
+	
+	
+	
+	
+	private static JFrame frame;
+	private JButton criarB;
+	private JButton eliminarB;
+	private JButton editarB;
+	
+	
+	public GUI() {
+		super(new BorderLayout());
+		frame = new JFrame("GUI");
+		frame.setSize(350, 350);
+		frame.setLocationRelativeTo(null);		
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		addFrameContent();
+
+	}
+	
+	
+	private void addFrameContent() {
+		
+		criarB = new JButton("create");
+		CreateListener createListener = new CreateListener(criarB);
+		criarB.setActionCommand("create");
+		criarB.addActionListener(createListener);
+		
+		
+		eliminarB = new JButton("delete");
+		EliminarListener eliminarListener = new EliminarListener(criarB);
+		eliminarB.setActionCommand("delete");
+		eliminarB.addActionListener(eliminarListener);
+		
+		editarB = new JButton("edit");
+		EditarListener editarListener = new EditarListener(editarB);
+		editarB.setActionCommand("edit");
+		editarB.addActionListener(editarListener);
+		
+		
+		//---LISTA JPANEL---
+		
+		JPanel buttonPane = new JPanel();
+		buttonPane.add(criarB);
+		buttonPane.add(eliminarB);
+		buttonPane.add(editarB);
+		
+		buttonPane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		add(buttonPane, BorderLayout.PAGE_END);
+	}
+	
+	public void open() {
+
+		frame.setContentPane(this);
+		frame.setVisible(true);
+
+	}
+	
+	class CreateListener implements ActionListener {
+
+		private JButton button;
+
+		
+		public CreateListener(JButton button) {
+			this.button = button;
+			
+
+		}
+
+		
+		
+		public void actionPerformed(ActionEvent arg0) {
+			GUI_janela gj = new GUI_janela("Create");
+			gj.open();
+			
+		}
+
+	}
+	
+	class EliminarListener implements ActionListener {
+
+		private JButton button;
+
+		
+		public EliminarListener(JButton button) {
+			this.button = button;
+
+		}
+
+		
+		
+		public void actionPerformed(ActionEvent arg0) {
+			//Pode se remover nesta janela
+			
+		}
+
+	}
+	
+	class EditarListener implements ActionListener {
+
+		private JButton button;
+
+		
+		public EditarListener(JButton button) {
+			this.button = button;
+
+		}
+
+		
+		
+		public void actionPerformed(ActionEvent arg0) {
+			GUI_janela gj = new GUI_janela("Edit");
+			gj.open();
+			
+		}
+
+	}
+	
+
+}
---
+++ esi/src/main/java/pt/iul/ista/esi/GUI_janela.java
@@ -0,0 +1,76 @@
+package pt.iul.ista.esi;
+
+import java.awt.BorderLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.WindowConstants;
+
+/*
+ * @author André Freitas
+ * 
+ */
+
+
+public class GUI_janela {
+	
+	String[] options = {"Yes","No"};
+	private JFrame frame;
+	private String name;
+	private JPanel panel;
+	private JLabel lbl;
+	
+
+	/*
+	 *  @param name - o nome da janela
+	 * 
+	 */
+	
+	public GUI_janela(String name) {
+		this.name = name;
+		frame = new JFrame(this.name);
+		panel = new JPanel();
+		lbl = new JLabel("Are you sure?");
+		panel.add(lbl);
+		frame.setLayout(new BorderLayout());
+		frame.setSize(300, 300);
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				if (JOptionPane.showOptionDialog(null, panel, "Close Panel", JOptionPane.NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, options , options[0]) == JOptionPane.YES_OPTION) {
+					frame.dispose();
+					
+				}
+			}
+		});
+		
+		addFrameContent();
+
+			
+	}
+
+	/*
+	 * Adicionar o conteudo aqui
+	 */
+
+	private void addFrameContent() {
+
+		
+		
+	}
+	
+	
+	
+	public void open() {
+		frame.setVisible(true);
+	}
+	
+}
