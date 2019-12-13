d3c2e0753d1a915fdc40ddce1372ee2f187be8ee
Miguel Diaz Gonçalves <mcdgs@iscte-iul.pt>
2019-12-13 17:51:13 +0000

    Javadoc

--- esi/src/test/java/pt/iul/ista/esi/AllTests.java
+++ esi/src/test/java/pt/iul/ista/esi/AllTests.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 @RunWith(JUnitPlatform.class)
 @SelectClasses({ AppTest.class, RegraTest.class, MetodoTest.class, ResultadoTest.class, MenuGuiTest.class,
 		EditorGuiTest.class, AvaliaGuiTest.class, GerirGuiTest.class, EscolheGuiTest.class, ComparaGuiTest.class,
-		FerramentaTest.class })
+		FerramentaTest.class, ValorEditorGuiTest.class })
 public class AllTests {
 
 }
