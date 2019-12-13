de7b5b332048c0d45b8a30903067e215ba13efea
miguel <miguel@miguel-Virtual-Machine>
2019-12-13 15:42:13 +0000

    Updated Javadoc da Class MenuGui

--- esi/src/main/java/pt/iul/ista/esi/MenuGui.java
+++ esi/src/main/java/pt/iul/ista/esi/MenuGui.java
@@ -1,21 +1,21 @@
 /**
-*
-* Trabalho final de Engenharia de Software I 2019/20
-* 
-* Grupo 85
-* 
-* Projecto Analise de erros de software.
-* 
-* Classe Gui
-*
-* @author Miguel Diaz Gonçalves 82493
-* @author Gonçalo Dias do Amaral 83380
-* @author Miguel Carriço 73745
-* @author André Freitas 82361
-* @author Pedro Jones 82946
-* @version 0.01
-*
-*/
+ *
+ * Trabalho final de Engenharia de Software I 2019/20
+ * 
+ * Grupo 85
+ * 
+ * Projecto Analise de erros de software.
+ * 
+ * Classe Gui
+ *
+ * @author Miguel Diaz Gonçalves 82493
+ * @author Gonçalo Dias do Amaral 83380
+ * @author Miguel Carriço 73745
+ * @author André Freitas 82361
+ * @author Pedro Jones 82946
+ * @version 0.01
+ *
+ */
 
 package pt.iul.ista.esi;
 
@@ -34,105 +34,101 @@ import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
-
-
-
 public class MenuGui extends JPanel{
-	
+
 	private static final long serialVersionUID = 1L;
 	private static JFrame frame;
 	private JButton gerirB;
 	private JButton avaliarB;
 	private JButton compararB;
 	private JButton escolherficheiroB;
-	
-	
+
+
 	/**
 	 * 
-	 * Construtor para classe GUI
+	 * Construtor para classe MenuGui
 	 * 
 	 */
-	
+
 	public MenuGui() {
 		super(new BorderLayout());
 		frame = new JFrame("MENU");
 		frame.setSize(700, 200);
 		frame.setLocationRelativeTo(null);	
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		
-		
-		addFrameContent();
 
+		addFrameContent();
 	}
-	
-	
+
+
 	/**
-	 * Adiciona conteúdo para GUI.
+	 * 
+	 * Metodo que adiciona conteúdo para GUI.
 	 */
-	
+
 	private void addFrameContent() {
-		
+
 		//---JLabel---
-		
+
 		JLabel lbl = new JLabel(App.TITLE,JLabel.CENTER );
-	
-		
+
+
 		//---JButton---
-		
+
 		gerirB = new JButton("Gerir regras");
 		GerirListener gerirListener = new GerirListener();
 		gerirB.setActionCommand("gerirRegras");
 		gerirB.addActionListener(gerirListener);
-		
+
 		avaliarB = new JButton("Avaliar metodos");
 		AvaliarListener avaliarListener = new AvaliarListener();
 		avaliarB.setActionCommand("avaliarMetodos");
 		avaliarB.addActionListener(avaliarListener);
-		
+
 		compararB = new JButton("Comparar aplicações");
 		CompararListener compararListener = new CompararListener();
 		compararB.setActionCommand("compararAvalicacoes");
 		compararB.addActionListener(compararListener);
-		
+
 		escolherficheiroB = new JButton("Escolher Ficheiro Excel");
 		EscolherficheiroListener escolherficheirolistener = new EscolherficheiroListener();
 		escolherficheiroB.setActionCommand("escolherficheiroexcel");
 		escolherficheiroB.addActionListener(escolherficheirolistener);
-		
-		
+
+
 		//---JPANEL---
-		
+
 		JPanel buttonPane = new JPanel();
 		buttonPane.add(gerirB);
 		buttonPane.add(avaliarB);
 		buttonPane.add(compararB);
 		buttonPane.add(escolherficheiroB);
-		
+
 		buttonPane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-		
+
 		add(lbl, BorderLayout.CENTER);
 		add(buttonPane, BorderLayout.PAGE_END);
-		
+
 	}
-	
+
 	/**
 	 * Abre a aplicação.
 	 */
-	
+
 	public void open() {
 
 		frame.setContentPane(this);
 		frame.setVisible(true);
 
 	}
-	
+
 	/**
 	 * Classe do botão gerir.
 	 */
-	
+
 	class GerirListener implements ActionListener {
 
-		
+
 		public void actionPerformed(ActionEvent arg0) {
 			SwingUtilities.invokeLater(new Runnable() {
 				public void run() {
@@ -143,14 +139,14 @@ public class MenuGui extends JPanel{
 		}
 
 	}
-	
+
 	/**
 	 * Classe do botão avaliar.
 	 */
-	
+
 	class AvaliarListener implements ActionListener {
 
-	
+
 		public void actionPerformed(ActionEvent arg0) {
 			SwingUtilities.invokeLater(new Runnable() {
 				public void run() {
@@ -159,34 +155,34 @@ public class MenuGui extends JPanel{
 				}
 			});
 		}
-		
+
 	}
-	
+
 	/**
 	 * Classe do botao escolher ficheiro.
 	 */
-	
+
 	class EscolherficheiroListener implements ActionListener {
 
-		
+
 		public void actionPerformed(ActionEvent arg0) {
 			SwingUtilities.invokeLater(new Runnable() {
 				public void run() {
 					EscolheGui ef = new EscolheGui();
-					
+
 				}
 			});
 		}
 
 	}
-	
+
 	/**
 	 * Classe do botão comparar.
 	 */
-	
+
 	class CompararListener implements ActionListener {
 
-		
+
 		public void actionPerformed(ActionEvent arg0) {
 			SwingUtilities.invokeLater(new Runnable() {
 				public void run() {
@@ -194,10 +190,10 @@ public class MenuGui extends JPanel{
 					gc.open();
 				}
 			});
-			
+
 		}
 
 	}
-	
+
 
 }
